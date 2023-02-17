c7a9490ca017c31946abae36a1b2523f46d5358d
Greatness <akumagreatness@gmail.com>
2023-02-17 11:54:31 +0100

    variable shadowing

--- variable.rs
+++ variable.rs
@@ -1,10 +1,14 @@
 fn main(){
     let long_lived_binding = 1;
     {
-        let short_lived_binding = 2;
+        let long_lived_binding = 2;
         
-        println!("innner short: {}", short_lived_binding);
+        println!("innner long: {}", long_lived_binding);
     }
-    //println!("outer short: {}", short_lived_binding)
-    println!("outer long: {}", long_lived_binding)
+    
+    println!("outer long: {}", long_lived_binding);
+    //outer shadowing of the existing variable
+    let long_lived_binding = 4;
+    println!("outer shadow: {}", long_lived_binding)
+
 }
