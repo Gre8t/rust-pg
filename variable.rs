f4601dbff7b464b3dc72f9a98382ea82f11a3953
Greatness <akumagreatness@gmail.com>
2023-02-17 12:05:55 +0100

    variable declare first and use later

--- variable.rs
+++ variable.rs
@@ -1,14 +1,21 @@
 fn main(){
-    let long_lived_binding = 1;
-    {
-        let long_lived_binding = 2;
-        
-        println!("innner long: {}", long_lived_binding);
-    }
-    
-    println!("outer long: {}", long_lived_binding);
-    //outer shadowing of the existing variable
-    let long_lived_binding = 4;
-    println!("outer shadow: {}", long_lived_binding)
+    //declare first
+    let a_binding;
 
+    {
+        let x = 2;
+        a_binding = x * x;
+    }    
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+// use a declared variable that is not yet initiallized
+    //println!("another binding:{}", another_binding)
+    another_binding = 1;
+// if you print here it should work fine without any errors
+    println!("another binding: {}", another_binding);
+    
+// This is a key difference with Go. Rust doesn't allow uninited 
+// variables to be used but you can do this in Go. Although Go
+// automatically set the value to nil, Rust doesn't allow it.
 }
